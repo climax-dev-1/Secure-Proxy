7181847769fc969d3675b294be7157233929cacc
climax-dev-1 <harold712.dev.work@gmail.com>
2025-09-07 18:11:26 +0200

    added method for getting JSON Values by Path: `dict.key`

--- utils/utils.go
+++ utils/utils.go
@@ -7,6 +7,8 @@ package utils
 
 import (
 	"encoding/json"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -28,6 +30,49 @@ func StringToArray(sliceStr string) []string {
     return items
 }
 
+func GetJsonByPath(path string, data interface{}) (interface{}, bool) {
+    // Split into parts by `.` and `[]`
+    re := regexp.MustCompile(`\.|\[|\]`)
+
+    parts := re.Split(path, -1)
+
+    cleaned := []string{}
+
+    for _, part := range parts {
+        if part != "" {
+            cleaned = append(cleaned, part)
+        }
+    }
+
+    current := data
+
+    for _, key := range cleaned {
+        switch currentDataType := current.(type) {
+            // Case: Dictionary
+            case map[string]interface{}:
+                value, ok := currentDataType[key]
+                if !ok {
+                    return nil, false
+                }
+                current = value
+
+            // Case: Array
+            case []interface{}:
+                index, err := strconv.Atoi(key)
+
+                if err != nil || index < 0 || index >= len(currentDataType) {
+                    return nil, false
+                }
+                current = currentDataType[index]
+
+            default:
+                return nil, false
+        }
+    }
+
+    return current, true
+}
+
 func GetJsonSafe[T any](jsonStr string) (T, error) {
 	var result T
 
@@ -46,4 +91,20 @@ func GetJson[T any](jsonStr string) (T) {
 	}
 
 	return result
+}
+
+func ToJsonSafe[T any](obj T) (string, error) {
+	bytes, err := json.Marshal(obj)
+
+	return string(bytes), err
+}
+
+func ToJson[T any](obj T) string {
+	bytes, err := json.Marshal(obj)
+
+	if err != nil {
+		// JSON is empty
+	}
+
+	return string(bytes)
 }
